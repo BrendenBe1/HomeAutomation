f6275c6a51d48c2da43965e0a22e3a4fea10d52b
Peter <peter_haschke@yahoo.com>
2016-11-07 17:16:02 -0700

    Added call to DB class

--- app/src/main/java/brendenbernal/homeautomation/AddLight.java
+++ app/src/main/java/brendenbernal/homeautomation/AddLight.java
@@ -9,10 +9,12 @@ import android.widget.EditText;
 
 public class AddLight extends AppCompatActivity {
 
+    DatabaseHelper myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_light);
+        myDB = new DatabaseHelper(this);
 
         onClick();
     }
@@ -44,6 +46,7 @@ public class AddLight extends AppCompatActivity {
             public void onClick (View v)
             {
                 // putExtra makes it so you can send variables or values to next activity
+                myDB.insertThermo(nameInput.getText().toString(), 67, "0", "0");
                 intent.putExtra("name", nameInput.getText().toString());
                 startActivity(intent);
             }
